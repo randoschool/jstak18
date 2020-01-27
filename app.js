6a5b3408acc5e78ce18e3836f5dc31c740ea8a9b
Rando Õispuu <rando.oispuu@ametikool.ee>
2020-01-27 11:32:46 +0200

    Tund

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const btns = Array.prototype.slice.call(document.querySelectorAll('.btn'));
 const answer = document.querySelector('#answer');
+let currentNumber = '';
+let firstNumber;
+let operator;
 
 btns.map(btn => btn.addEventListener('click', (evt) => {
-    answer.innerText = evt.target.innerText
+    if (! isNaN(parseInt(evt.target.innerText))) {
+        saveCurrentNumber(evt.target.innerText);
+    }
+
+    if (isNaN(parseInt(evt.target.innerText))){
+        activateOperator(evt);
+    }
 }));
 
+function saveCurrentNumber(number) {
+    currentNumber += number
+    updateDisplay();
+}
+
+function updateDisplay(){
+    answer.innerText = currentNumber
+}
+
+function activateOperator(evt) {
+    btns.map(btn => btn.classList.remove('active'));
+    evt.target.classList.add('active');
+    answer.innerText = '';
+    firstNumber = parseInt(currentNumber)
+    currentNumber = '';
+
+    if(evt.target.innerText !== 'Arvuta') {
+        operator = getOperator(evt.target.innerText)
+    }
+
+    if(evt.target.innerText === 'Arvuta') {
+        answer.innerText = eval(firstNumber + operator  + currentNumber) 
+    }
+
+}
